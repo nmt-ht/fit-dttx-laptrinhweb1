77be4f9654a3e367bed46a16c2050ae8a5fc79c3
Tu Nguyen <nmt.hitu@gmail.com>
2022-04-11 23:44:43 +0700

    lap trinh web 1 - lab05 - Javascript basic

---
+++ lab05/js/Ex09.js
@@ -0,0 +1,6 @@
+function checkAll(checked){
+    var checkboxes = document.getElementsByTagName("input");
+    for(i = 0; i < checkboxes.length; i++){
+        checkboxes[i].checked = checked;
+    }
+}
